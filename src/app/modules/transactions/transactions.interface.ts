613970a5a260e8c2582d4e085126a08fb393ea0a
Md Moniruzzaman <web.moniruzzaman1@gmail.com>
2025-02-28 22:57:01 +0600

    transaction interface and model done

---
+++ src/app/modules/transactions/transactions.interface.ts
@@ -0,0 +1,9 @@
+import { Types } from "mongoose";
+
+export interface TTransaction {
+    _id?: Types.ObjectId;
+    buyerID: Types.ObjectId;
+    sellerID: Types.ObjectId;
+    itemID: Types.ObjectId;
+    status?: 'pending' | 'completed';
+}
