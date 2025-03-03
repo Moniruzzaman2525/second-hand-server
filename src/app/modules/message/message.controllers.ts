edffc3dafaab219562bed4ac59568e910387166a
Md Moniruzzaman <web.moniruzzaman1@gmail.com>
2025-03-03 16:17:24 +0600

    services and controller done

--- src/app/modules/message/message.controllers.ts
+++ src/app/modules/message/message.controllers.ts
@@ -3,14 +3,12 @@ import catchAsync from "../../utils/catchAsync";
 import { IJwtPayload } from "../auth/auth.interface";
 import sendResponse from "../../utils/sendResponse";
 import { StatusCodes } from "http-status-codes";
+import { messageServices } from "./message.services";
 
 
 
 const getAllMessage = catchAsync(async (req: Request, res: Response) => {
-    const result = await ProductService.createProduct(
-        req.user as IJwtPayload
-    );
-
+    const result = await messageServices.getAllMessage(req.user as IJwtPayload);
     sendResponse(res, {
         statusCode: StatusCodes.OK,
         success: true,
