13401d363822a9e9ed9d3a388f7f104337a9862f
Md Moniruzzaman <web.moniruzzaman1@gmail.com>
2025-03-05 17:13:57 +0600

    contact us controller done

---
+++ src/app/modules/contact/contact.controller.ts
@@ -0,0 +1,22 @@
+import { Request, Response } from "express";
+import catchAsync from "../../utils/catchAsync";
+import sendResponse from "../../utils/sendResponse";
+import { StatusCodes } from "http-status-codes";
+
+
+
+const contactUs = catchAsync(async (req: Request, res: Response) => {
+    const { name, email, phone, message } = req.body;
+    const result = await contactServices.contactUs(name, email, phone, message);
+
+    sendResponse(res, {
+        statusCode: StatusCodes.CREATED,
+        success: true,
+        message: "Message sent successfully",
+        data: result,
+    });
+});
+
+export const contactController = {
+    contactUs
+}
